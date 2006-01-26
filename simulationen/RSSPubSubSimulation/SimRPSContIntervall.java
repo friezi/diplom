42ad9571c5f2ba97a11618030e620136dd203672
friezi <>
2006-01-26 17:47:15 +0000

    noch mehr

--- simulationen/RSSPubSubSimulation/SimRPSContIntervall.java
+++ simulationen/RSSPubSubSimulation/SimRPSContIntervall.java
@@ -2,18 +2,30 @@ import rsspubsubframework.*;
 
 public class SimRPSContIntervall {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-        Szenario1 szenario = new Szenario1(new RPSFactory() {
-            public BrokerNode newBrokerNode(int xp, int yp){ return new Broker(xp,yp); }
-            public PubSubNode newPubSubNode(int xp, int yp){ return new PubSub(xp,yp); }
-            public RSSServerNode newRSSServerNode(int xp, int yp){ return new RSSServer(xp,yp); }
-        });
+		Szenario1 szenario = new Szenario1(new RPSFactory() {
+			public BrokerNode newBrokerNode(int xp, int yp) {
+				return new Broker(xp, yp);
+			}
 
-    }
+			public PubSubNode newPubSubNode(int xp, int yp) {
+				return new PubSub(xp, yp);
+			}
+
+			public RSSServerNode newRSSServerNode(int xp, int yp) {
+				return new RSSServer(xp, yp);
+			}
+		}, new ColorFeedFactory(), new RSSFeedRepresentationFactory() {
+			public RSSFeedRepresentation newRSSFeedRepresentation(
+					DisplayableObject dObj, RSSFeed feed) {
+				return new ColorFeedRepresentation(dObj, (ColorFeed) feed);
+			}
+		});
+	}
 
 }
