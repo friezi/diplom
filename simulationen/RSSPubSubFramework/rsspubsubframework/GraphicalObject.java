541a0edb354427bb7a8b820f4dd7afbcbd1cb416
friezi <>
2006-03-27 18:55:47 +0000

    graphical objects

---
+++ simulationen/RSSPubSubFramework/rsspubsubframework/GraphicalObject.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package rsspubsubframework;
+
+import java.awt.*;
+
+/**
+ * @author friezi
+ *
+ */
+public abstract class GraphicalObject extends DisplayableObject {
+
+	/**
+	 * 
+	 */
+	public GraphicalObject() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	abstract void drawobj(Graphics g);
+
+}
