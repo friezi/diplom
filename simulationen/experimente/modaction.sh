013668064d1a9399a207f7a98237e600abb2e8d0
friezi <>
2006-10-22 13:52:34 +0000

    processing-times

--- simulationen/experimente/modaction.sh
+++ simulationen/experimente/modaction.sh
@@ -6,7 +6,9 @@ do
 	for file in mac*
 	do
 	echo $file
-		sed -i .bak -e 's/seedValue=\(.*\)/seedValue=-7847005838091513385/g' $file
+#		sed --in-place=.bak -e 's/seedValue=\(.*\)/seedValue=-7847005838091513385/g' $file
+#		sed --in-place=.bak -e 's/processingTimeFeedRequests\(.*\)/\n/g' -e 's/processingTimeUnrepliedRequests\(.*\)/\n/g' -e 's/saveProperties\(.*\)=\(.*\)false/saveProperties=true/g' $file
+		sed --in-place=.bak -e 's/saveProperties\(.*\)=\(.*\)true/saveProperties=false/g' $file
 #		rm -f *.bak
 	done
 	cd ..
