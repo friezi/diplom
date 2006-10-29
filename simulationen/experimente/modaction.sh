bfc4a7f10fcce0093b6f7d5ab9683ca9cabb4fda
friezi <>
2006-10-29 17:14:55 +0000

    BRITEBrokernetFile

--- simulationen/experimente/modaction.sh
+++ simulationen/experimente/modaction.sh
@@ -3,12 +3,12 @@ mod=`pwd`/modifyContent.sh
 for dir in $*
 do
 	cd $dir
-	for file in mac*
+	for file in *.sim
 	do
 	echo $file
 #		sed --in-place=.bak -e 's/seedValue=\(.*\)/seedValue=-7847005838091513385/g' $file
 #		sed --in-place=.bak -e 's/processingTimeFeedRequests\(.*\)/\n/g' -e 's/processingTimeUnrepliedRequests\(.*\)/\n/g' -e 's/saveProperties\(.*\)=\(.*\)false/saveProperties=true/g' $file
-		sed --in-place=.bak -e 's/saveProperties\(.*\)=\(.*\)true/saveProperties=false/g' $file
+		sed --in-place=.bak -e 's/BRITEToplayerFile\(.*\)=\(.*\)/BRITEBrokernetFile=\2/g' $file
 #		rm -f *.bak
 	done
 	cd ..
