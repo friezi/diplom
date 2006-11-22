374a1ef67f39f972e6d01a73cf076b0214d4263e
friezi <>
2006-11-22 12:05:06 +0000

    SinglePublisher

---
+++ simulationen/experimente/SinglePublisher/ps_ToTR_SinglePublisher_MVR.gnuplot
@@ -0,0 +1,11 @@
+set terminal postscript
+unset parametric
+set yrange [-12:200]
+set xlabel "Time/secs"
+set ylabel "RSSFeed-Requests"
+plot 'totalTemporaryRequests_SinglePublisher_MeanValueRanges.gnuplotdata' w l
+load 'queue.gnuplot'
+load 'markers.gnuplot'
+replot queue
+set output 'ToTR_SinglePublisher_MVR.ps'
+replot
--- simulationen/experimente/modaction.sh
+++ simulationen/experimente/modaction.sh
@@ -3,13 +3,13 @@ mod=`pwd`/modifyContent.sh
 for dir in $*
 do
 	cd $dir
-	for file in *.sim
+	for file in *
 	do
 	echo $file
 #		sed --in-place=.bak -e 's/seedValue=\(.*\)/seedValue=-7847005838091513385/g' $file
 #		sed --in-place=.bak -e 's/processingTimeFeedRequests\(.*\)/\n/g' -e 's/processingTimeUnrepliedRequests\(.*\)/\n/g' -e 's/saveProperties\(.*\)=\(.*\)false/saveProperties=true/g' $file
-		sed --in-place=.bak -e 's/BRITEToplayerFile\(.*\)=\(.*\)/BRITEBrokernetFile=\2/g' $file
-#		rm -f *.bak
+		sed --in-place=.bak -e 's/Referenzverlauf/SinglePublisher/g' $file
+		rm -f *.bak
 	done
 	cd ..
 done
