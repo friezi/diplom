e1a3cb6753f7baa1bd2708943b7dd876c38a7e69
friezi <>
2006-12-11 19:46:08 +0000

    kticks

--- simulationen/experimente/modaction.sh
+++ simulationen/experimente/modaction.sh
@@ -14,7 +14,7 @@ do
 	echo $file
 #		sed --in-place=.bak -e 's/seedValue=\(.*\)/seedValue=-7847005838091513385/g' $file
 #		sed --in-place=.bak -e 's/processingTimeFeedRequests\(.*\)/\n/g' -e 's/processingTimeUnrepliedRequests\(.*\)/\n/g' -e 's/saveProperties\(.*\)=\(.*\)false/saveProperties=true/g' $file
-		sed --in-place=.bak -e 's/Time\/secs/Time\/ticks/g' $file
+		sed --in-place=.bak -e 's/Time\/secs/Time\/kticks/g' $file
 #		rm -f *.bak
 	done
 	popd
